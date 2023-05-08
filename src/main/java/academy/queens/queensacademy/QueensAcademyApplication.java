15484ee2e87822fd06724ff0145e3ba11eade741
LordNedStark <osisiogubenjamin1@gmail.com>
2023-05-08 09:21:33 +0100

    first commit

---
+++ src/main/java/academy/queens/queensacademy/QueensAcademyApplication.java
@@ -0,0 +1,13 @@
+package academy.queens.queensacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueensAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueensAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/queens/queensacademy/QueensAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.queens.queensacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueensAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
